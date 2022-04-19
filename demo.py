a6d1b5dc2aa87f1bb298ede30c70695ec2da8ec6
Alex Clark <aclark@aclark.net>
2022-04-19 09:39:35 -0400

    A one time black event

--- demo.py
+++ demo.py
@@ -1,6 +1,21 @@
-# Via https://docs.pymc.io/en/v3/
 import pymc3 as pm
+import csv
+import requests
 
+# Via https://stackoverflow.com/a/35371451
+CSV_URL = 'http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv'
+with requests.Session() as s:
+    download = s.get(CSV_URL)
+
+    decoded_content = download.content.decode('utf-8')
+
+    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    my_list = list(cr)
+    for row in my_list:
+        print(row)
+
+
+# Via https://docs.pymc.io/en/v3/
 X, y = linear_training_data()
 with pm.Model() as linear_model:
     weights = pm.Normal("weights", mu=0, sigma=1)
