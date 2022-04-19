73b5ce9871e5ecf4ced43c3ec48d9939faf6d0e5
Alex Clark <aclark@aclark.net>
2022-04-19 12:14:40 -0400

    Update demo2

--- demo2.py
+++ demo2.py
@@ -1,18 +1,18 @@
-import pandas as pd
+# Via https://towardsdatascience.com/introduction-to-linear-regression-in-python-c12a072bedf0
 import numpy as np
+import pandas as pd
 from matplotlib import pyplot as plt
 
 # Generate 'random' data
 np.random.seed(0)
-X = 2.5 * np.random.randn(100) + 1.5   # Array of 100 values with mean = 1.5, stddev = 2.5
-res = 0.5 * np.random.randn(100)       # Generate 100 residual terms
-y = 2 + 0.3 * X + res                  # Actual values of Y
+X = (
+    2.5 * np.random.randn(100) + 1.5
+)  # Array of 100 values with mean = 1.5, stddev = 2.5
+res = 0.5 * np.random.randn(100)  # Generate 100 residual terms
+y = 2 + 0.3 * X + res  # Actual values of Y
 
 # Create pandas dataframe to store our X and y values
-df = pd.DataFrame(
-    {'X': X,
-     'y': y}
-)
+df = pd.DataFrame({"X": X, "y": y})
 
 # Show the first five rows of our dataframe
 print(df.head())
