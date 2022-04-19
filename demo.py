e91756ff81344fb67a12d458772ddd7b95576278
Alex Clark <aclark@aclark.net>
2022-04-19 09:40:28 -0400

    A one time black event

--- demo.py
+++ demo.py
@@ -3,13 +3,13 @@ import csv
 import requests
 
 # Via https://stackoverflow.com/a/35371451
-CSV_URL = 'http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv'
+CSV_URL = "http://samplecsvs.s3.amazonaws.com/Sacramentorealestatetransactions.csv"
 with requests.Session() as s:
     download = s.get(CSV_URL)
 
-    decoded_content = download.content.decode('utf-8')
+    decoded_content = download.content.decode("utf-8")
 
-    cr = csv.reader(decoded_content.splitlines(), delimiter=',')
+    cr = csv.reader(decoded_content.splitlines(), delimiter=",")
     my_list = list(cr)
     for row in my_list:
         print(row)
